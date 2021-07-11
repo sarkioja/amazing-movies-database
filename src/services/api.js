f719047fe0f8d5f0ccd8336e278438bc341a94de
Renan Cunha <sarkioja@gmail.com>
2021-07-11 10:20:58 -0300

    Consuming environmnent variables in vercel

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: `https://api.themoviedb.org/3/`
 });
 
-const token = process.env.TMDB_API_KEY
+const token = process.env.REACT_APP_TMDB_API_KEY
 api.defaults.headers.authorization = `Bearer ${token}`;
 
 export default api;
