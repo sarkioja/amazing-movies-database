b706c336fdec21d07d9f1bba950293fceebb5c81
Renan Cunha <sarkioja@gmail.com>
2021-07-12 14:38:38 -0300

    Testing new env setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,46 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const dotenv = require('dotenv')
+const Dotenv = require('dotenv-webpack');
 
-module.exports = () => {
-    const env = dotenv.config().parsed,
-    envKeys = Object.keys(env).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env[next]);
-        return prev;
-    }, {});
-
-    return {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'bundle.js'
-        },
-        module: {
-            rules: [
-                { 
-                    test: /\.(js)$/, 
-                    use: 'babel-loader' 
-                },
-                { 
-                    test: /\.css$/, 
-                    use: ['style-loader', 'css-loader'] 
-                }, 
-                {
-                    test: /\.(png|jp(e*)g|svg|gif)$/,
-                    use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                        name: 'images/[hash]-[name].[ext]',
-                        },
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.(js)$/, 
+                use: 'babel-loader' 
+            },
+            { 
+                test: /\.css$/, 
+                use: ['style-loader', 'css-loader'] 
+            }, 
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: 'images/[hash]-[name].[ext]',
                     },
-                    ],
-                },
-            ]
-        },
-        mode: 'development',
-        plugins: [
-            new webpack.DefinePlugin(envKeys),
-            new HtmlWebpackPlugin({
-                template: 'src/static/index.html'
-            })
-        ],
-        devServer: {
-            historyApiFallback: true,
-            contentBase: './',
-            hot: true
-        },
-    }
+                  },
+                ],
+            },
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new Dotenv(),
+        new HtmlWebpackPlugin({
+            template: 'src/static/index.html'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './',
+        hot: true
+      },
 }
